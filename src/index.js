803a5b73ca88203664bd8f665102879875dfe2e9
Adrian Balan <adrianbalan1996@icloud.com>
2023-05-20 21:53:22 +0300

    Fix: Runtime automatic

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import React from "react"
 
 import "./index.css";
 import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>);
